ffdaac22d4121f66aaf1564248bef52c4cd34fe6
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2024-02-21 23:22:06 -0300

    Update calculaValorEstacionamento.java
    adicionei minutos no sopln

--- Estruturas Condicionais/calculaValorEstacionamento.java
+++ Estruturas Condicionais/calculaValorEstacionamento.java
@@ -9,21 +9,22 @@ public class calculaValorEstacionamento {
         tempoPermanencia = dado.nextInt();
 
         int horas = tempoPermanencia / 60;
+        int minutos = tempoPermanencia % 60;
 
         if (tempoPermanencia <= 60) {
-            System.out.println("Tempo Estacionado: " + horas + " Hora(s) ou " + tempoPermanencia + " Minutos");
+            System.out.println("Tempo Estacionado: " + horas + " Hora(s) e " + minutos + " minutos ou " + tempoPermanencia + " Minutos");
             System.out.println("Valor do Estacionamento: R$5,00");
         }
         if (tempoPermanencia > 60 && tempoPermanencia < 120 ) {
-            System.out.println("Tempo Estacionado: " + horas + " Hora(s) ou " + tempoPermanencia + " Minutos");
+            System.out.println("Tempo Estacionado: " + horas + " Hora(s) e " + minutos + " minutos ou " + tempoPermanencia + " Minutos");
             System.out.println("Valor do Estacionamento: R$8,00");
         }
         if (tempoPermanencia >= 120 && tempoPermanencia < 240) {
-            System.out.println("Tempo Estacionado: " + horas + " Hora(s) ou " + tempoPermanencia + " Minutos");
+            System.out.println("Tempo Estacionado: " + horas + " Hora(s) e " + minutos + " minutos ou " + tempoPermanencia + " Minutos");
             System.out.println("Valor do Estacionamento: R$12,00");
         }
         if (tempoPermanencia >= 240) {
-            System.out.println("Tempo Estacionado: " + horas + " Hora(s) ou " + tempoPermanencia + " Minutos");
+            System.out.println("Tempo Estacionado: " + horas + " Hora(s) e " + minutos + " minutos ou " + tempoPermanencia + " Minutos");
             System.out.println("Valor do Estacionamento: R$15,00");
         }
     }
